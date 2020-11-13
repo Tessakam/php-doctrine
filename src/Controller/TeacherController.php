bda831a0d02c1fdda45a5e06121e594b2876b1c8
Tessakam <tessa.kam@hotmail.com>
2020-11-13 12:37:36 +0100

    created same structure for teacher - trying to get all teachers now

---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Address;
+use App\Entity\Teacher;
+use App\Repository\TeacherRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class TeacherController extends AbstractController
+{
+    /**
+     * @Route("/teacher", name="teacher")
+     * @param Request $request
+     * @return JsonResponse
+     */
+
+    private TeacherRepository $teacherRepository;
+
+    /**
+     * TeacherController constructor.
+     * @param TeacherRepository $teacherRepository
+     */
+    public function __construct(TeacherRepository $teacherRepository)
+    {
+        $this->teacherRepository = $teacherRepository;
+    }
+
+    public function add(Request $request): JsonResponse
+    {
+        // json_decode ( string $json [, bool $assoc = NULL [, int $depth = 512 [, int $options = 0 ]]] ) : mixed
+        // getContent links with studentRepository
+        $data = json_decode($request->getContent(), true, 512);
+
+        //embedded address
+        $address = new Address($data['address']['street'], $data['address']['streetNumber'], $data['address']['zip'], $data['address']['zipcode']);
+
+        $teacher = new teacher();
+        $teacher->setFirstName($data['firstName']);
+        $teacher->setLastName($data['lastName']);
+        $teacher->setEmail($data['email']);
+        $teacher->setAddress($address);
+        $this->getDoctrine()->getManager()->persist($teacher);
+        $this->getDoctrine()->getManager()->flush();
+        return new JsonResponse();
+
+        var_dump(json_decode($data));
+        //json_decode($data,JSON_PRETTY_PRINT);
+    }
+
+
+}
+
+/*public function index(): Response
+{
+    return $this->render('teacher/index.html.twig', [
+        'controller_name' => 'TeacherController',
+    ]);
+}*/
